48d41051694b84146b9426c14da859d00bea3848
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2019-06-28 00:39:26 +0800

    test loader #3: removed timeout

--- src/js/index.js
+++ src/js/index.js
@@ -9,20 +9,18 @@ import App from './App';
  * also fully loaded
  */
 window.onload = () => {
-    setTimeout(function () {
-        // Initialize Element extension
-        Query.extend();
+    // Initialize Element extension
+    Query.extend();
 
-        // Image for the ImageParticles
-        const logoCut = '/img/logo-cut.png';
-        // The App object contains all the logic and actions of the website.
-        // The purpose of this file (index.js) is to initialize
-        const app = new App();
-        // Set the image of the particles
-        app.image = logoCut;
-        // Date I started working
-        app.startDate = new Date(2014, 10);
-        // Initialize app
-        app.init();
-    }, 5000);
+    // Image for the ImageParticles
+    const logoCut = '/img/logo-cut.png';
+    // The App object contains all the logic and actions of the website.
+    // The purpose of this file (index.js) is to initialize
+    const app = new App();
+    // Set the image of the particles
+    app.image = logoCut;
+    // Date I started working
+    app.startDate = new Date(2014, 10);
+    // Initialize app
+    app.init();
 }
