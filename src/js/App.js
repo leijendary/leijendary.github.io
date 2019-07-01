d707ede8ba1d237dd4066fb81fdbf049229b9894
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2019-07-02 00:35:41 +0800

    updated scroll text and its responsiveness

--- src/js/App.js
+++ src/js/App.js
@@ -311,7 +311,7 @@ export default class App {
          */
         function updateImage(image) {
             bg.style.backgroundImage =
-                "radial-gradient(ellipse at center, rgba(0,0,0,0.6) 10%, rgba(0,0,0,0.8) 100%), " +
+                "radial-gradient(ellipse at center, rgba(0,0,0,0.7) 30%, rgba(0,0,0,0.9) 100%), " +
                 "url('" + image + "')";
         }
 
